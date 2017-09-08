073c1ad2766ffe10d7ecc7e5639646e3d4804697
jailbirt <jailbirt@gmail.com>
2017-09-08 20:19:10 -0300

    Some fixes

--- scripts/handleEBS.sh
+++ scripts/handleEBS.sh
@@ -34,7 +34,7 @@ do
       ;;
     -d=*|--days=*)
       AGE="${i#*=}"
-      if ! [[ $AGE =~ '^[0-9]+$' ]] ; then
+      if ! [[ $AGE =~ ^[0-9]+$ ]] ; then
         echo "Age is invalid Asuming default 7"
         AGE=7
             fi
@@ -53,34 +53,34 @@ done
 #functions
 function backup_ebs () {
    
-   echo finding TAG:$TAG for performing $ACTION
+   echo "finding TAG:$TAG for performing $ACTION"
    
    for instance in $(aws ec2 describe-instances --filters "Name=tag-value,Values=$TAG" | jq -r ".Reservations[].Instances[].InstanceId")
    do
      echo "$instance matches"
-     volumes=`aws ec2 describe-volumes --filter Name=attachment.instance-id,Values=$instance | jq .Volumes[].VolumeId | sed 's/\"//g'`
+     volumes=$(aws ec2 describe-volumes --filter Name=attachment.instance-id,Values="$instance" | jq .Volumes[].VolumeId | sed 's/\"//g')
      
      for volume in $volumes
      do
-       echo Creating snapshot for $volume $(aws ec2 create-snapshot --volume-id $volume --description "$TAG ebs-backup-script")
+       echo "Creating snapshot for $volume $(aws ec2 create-snapshot --volume-id "$volume" --description "$TAG ebs-backup-script")"
      done
      
    done
 }
 
 function attach_ebs () {
-     echo finding newest snapshot for provided TAG:$TAG for performing $ACTION
+     echo "finding newest snapshot for provided TAG:$TAG for performing $ACTION"
      snapshot=$(aws ec2 describe-snapshots --filters Name=description,Values="$TAG ebs-backup-script" | jq '.[]|max_by(.StartTime)|.SnapshotId' | sed 's/\"//g')
-     echo finding availability zone for $INSTANCEID
+     echo finding availability zone for "$INSTANCEID"
      echo "Todo Add IOPS before volume creation"
-     availabilityZone=$(aws ec2 describe-instances --instance-ids $INSTANCEID|jq .Reservations[].Instances[].Placement.AvailabilityZone| sed 's/\"//g')
-     volumeID=$(aws ec2 create-volume --region us-east-1 --availability-zone $availabilityZone --snapshot-id $snapshot|jq .VolumeId |sed 's/\"//g' )
-     echo waiting for volume $volumeID to become available
-     aws ec2 wait volume-available --volume-ids $volumeID
-     aws ec2 attach-volume --volume-id $volumeID --instance-id $INSTANCEID --device /dev/sdn
+     availabilityZone=$(aws ec2 describe-instances --instance-ids "$INSTANCEID"|jq .Reservations[].Instances[].Placement.AvailabilityZone| sed 's/\"//g')
+     volumeID=$(aws ec2 create-volume --region us-east-1 --availability-zone "$availabilityZone" --snapshot-id "$snapshot"|jq .VolumeId |sed 's/\"//g' )
+     echo waiting for volume "$volumeID "to become available
+     aws ec2 wait volume-available --volume-ids "$volumeID"
+     aws ec2 attach-volume --volume-id "$volumeID" --instance-id "$INSTANCEID" --device /dev/sdn
      #aws ec2 mount snapshot. 
      echo Verifing Volumes
-     aws ec2 describe-volumes --filter Name=attachment.instance-id,Values=$INSTANCEID | jq .Volumes[].VolumeId | sed 's/\"//g'
+     aws ec2 describe-volumes --filter Name=attachment.instance-id,Values="$INSTANCEID" | jq .Volumes[].VolumeId | sed 's/\"//g'
 }
 
 function delete_snapshots () {
@@ -88,15 +88,15 @@ function delete_snapshots () {
    for snapshot in $(aws ec2 describe-snapshots --filters Name=description,Values="$TAG ebs-backup-script" | jq .Snapshots[].SnapshotId | sed 's/\"//g')
    do
    
-   SNAPSHOTDATE=$(aws ec2 describe-snapshots --filters Name=snapshot-id,Values=$snapshot | jq .Snapshots[].StartTime | cut -d T -f1 | sed 's/\"//g')
+   SNAPSHOTDATE="$(aws ec2 describe-snapshots --filters Name=snapshot-id,Values="$snapshot" | jq .Snapshots[].StartTime | cut -d T -f1 | sed 's/\"//g')"
    STARTDATE=$(date +%s)
-   ENDDATE=$(date -d $SNAPSHOTDATE +%s)
-   INTERVAL=$[ (STARTDATE - ENDDATE) / (60*60*24) ]
+   ENDDATE="$(date -d "$SNAPSHOTDATE" +%s)"
+   INTERVAL=$(( (STARTDATE - ENDDATE) / (60*60*24) ))
    
    if (( $INTERVAL >= $AGE ));
    then
    echo "Deleting snapshot --> $snapshot"
-   aws ec2 delete-snapshot --snapshot-id $snapshot
+   aws ec2 delete-snapshot --snapshot-id "$snapshot"
    fi
    
    done
@@ -119,7 +119,7 @@ case $ACTION in
 ;;
 
 "attach")
-  if [ -z $INSTANCEID ];
+  if [ -z "$INSTANCEID" ];
   then
     echo "--instance=ID undefined, please pick one of these:"
     aws ec2 describe-instances | jq -r ".Reservations[].Instances[].InstanceId"
@@ -129,7 +129,7 @@ case $ACTION in
 ;;
 
 *)
-  help
+  help "$@"
 ;;
 
 esac 
