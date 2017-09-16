bb10b114d17f5715eba4a76e03ff49d0728f04fb
jailbirt <jailbirt@gmail.com>
2017-09-16 15:42:01 -0300

    targetGroups.sh

---
+++ scripts/targetGroups.sh
@@ -0,0 +1,20 @@
+echo use by: $0 arnID numberOfDesiredInstances
+if [ -z $1 ];then
+	echo "Target group ARN wasnt defined"
+	exit 1
+fi
+number=3
+if [ -z $2 ];then
+        echo "Number of healthy instances wasnt defined using 3 by default"
+else 
+number=$2
+fi
+
+healthyInstances=$(aws elbv2 describe-target-health --target-group-arn $1|jq .TargetHealthDescriptions[].Target.Id|wc -l)
+status="normal"
+if [ $number -gt $healthyInstances ];then
+ status="failure"
+fi
+echo "$healthyInstances are running at $1 TargetGroup"
+echo "$status"
+
